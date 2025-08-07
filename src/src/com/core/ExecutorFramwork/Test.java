43438bf4af4f046fb7826dc9a4d04f4e5153eba4
SagarDahibhate <sagardahibhate1@gmail.com>
2025-08-07 14:43:39 +0530

    added parent thrad cod

--- src/src/com/core/ExecutorFramwork/Test.java
+++ src/src/com/core/ExecutorFramwork/Test.java
@@ -24,7 +24,9 @@ public class Test {
 
 	public static void main(String[] args) {
 
-      System.out.println(Thread.currentThread().getThreadGroup().getName());
+      System.out.println(Thread.currentThread().getThreadGroup().getName()); // main 
+      
+      System.out.println(Thread.currentThread().getThreadGroup().getParent().getName()); // system
       
 	}
 
