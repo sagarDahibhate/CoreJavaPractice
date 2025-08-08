92c39b1e7f117b9d69cebd5988a0fbaac0c36246
SagarDahibhate <sagardahibhate1@gmail.com>
2025-08-08 11:10:56 +0530

    definition added

--- src/src/com/core/ExecutorFramwork/Test3.java
+++ src/src/com/core/ExecutorFramwork/Test3.java
@@ -1,5 +1,26 @@
 package src.com.core.ExecutorFramwork;
 
+/*
+ * Lock -->  
+ *    the problem with traditional synchronized keyword is 
+ *    1)   we are not having any flexiblity to try for a lock without waiting
+ *    2)    there is no way to specify max waiting time for a thread to get lock . so that thread will wait until getting the lock . which may creats performance problem which may cause deadline
+ *    3) if a thread releases the lock then which thread will get lock we are not having any control on this .
+ *    4) there is no api to list out all waiting threads for a lock
+ *    5)  the synchronized keyword compulsury we have to use at method or with in  method and its not possible to use accross multiple methods .
+ *    6) to overcome above issues  we can use  [ java.util.concurrent.lock Interface]
+ *    7) it provides several enhancement to the progamer to provide more control on concurrency
+ *    
+ *      methods -
+ *      void lock()   ,  boolean tryLock()  , void lockInterruptability()  , void unLock()
+ *    
+ * */
+
+
+
+
+
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Test3 {
@@ -32,3 +53,13 @@ public class Test3 {
 	}
 
 }
+
+
+
+/*
+ * Reentrant Lock -->  
+ *                  Means a thread can aquire same lock multiple times without any issue internally reentrant lock increment threads  personal count
+ *                  whenever we call lock method decrement count value , whenever we call unlock method & lock will be released wheever count reaches zero 
+ *                   
+ * */
+ 
